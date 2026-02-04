fe8931cfe39de9e4b153a1ef4046323cfca90f4d
SamTougherKainos <samuel.tougher@Kainos.com>
2026-02-04 15:49:09 +0000

    update address size for pr

--- database/Member.sql
+++ database/Member.sql
@@ -5,7 +5,7 @@ CREATE TABLE Member (
     phone_num VARCHAR(20),
     email_address VARCHAR(255),
     address_line1 VARCHAR(4),
-    address_line2 VARCHAR(20),
+    address_line2 VARCHAR(255),
     postal_code VARCHAR(10),
     membership_status VARCHAR(50),
     registration_date TIMESTAMP null
