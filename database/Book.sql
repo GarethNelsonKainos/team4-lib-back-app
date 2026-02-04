b32b54ee416f102765d67b781c2c0e343bef7c6f
SamTougherKainos <samuel.tougher@Kainos.com>
2026-02-04 16:25:53 +0000

    update to relational data in books

--- database/Book.sql
+++ database/Book.sql
@@ -1,9 +1,8 @@
 CREATE TABLE Book (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
     isbn VARCHAR(20) UNIQUE NOT NULL,
-    genre VARCHAR(100),
+    genre_id INT REFERENCES Genre(id),
     publication_year INT,
     blurb TEXT
 );
