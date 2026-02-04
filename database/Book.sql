8d3c8171fce40a43c43c93b10282e8eebacd9dcd
nick-parke <nicholas.parke@kainos.com>
2026-02-04 14:26:51 +0000

    Remove protected keywords from SQL files

--- database/Book.sql
+++ database/Book.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Book (
-    id SERIAL PRIMARY KEY,
+    book_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     isbn VARCHAR(20) UNIQUE NOT NULL,
     genre VARCHAR(100),
     publication_year INT,
-    description TEXT
+    blurb TEXT
 );
--- database/Copy.sql
+++ database/Copy.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Copy (
-    id SERIAL PRIMARY KEY,
+    copy_id SERIAL PRIMARY KEY,
     book_id INT NOT NULL REFERENCES Book(id) ON DELETE CASCADE,
     copy_id VARCHAR(50) UNIQUE NOT NULL,
-    status VARCHAR(20) CHECK (status IN ('Available', 'Borrowed')) NOT NULL,
+    copy_status VARCHAR(20) CHECK (status IN ('Available', 'Borrowed')) NOT NULL,
     borrower_id INT REFERENCES Member(id),
-    current_borrow_date DATE
+    borrow_id INT REFERENCES Borrow(borrow_id)
 );
