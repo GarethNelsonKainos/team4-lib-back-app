6dc4ce88005912bd6ec0a8d00ab2de971f72ff7a
lauren5656 <lauren.wilson@kainos.com>
2026-02-04 14:14:55 +0000

    Update Borrow.sql

--- database/Borrow.sql
+++ database/Borrow.sql
@@ -3,8 +3,7 @@ CREATE TABLE Borrow (
     member_id INT NOT NULL REFERENCES Member(id),
     copy_id INT NOT NULL REFERENCES Copy(id),
     borrow_date DATE NOT NULL,
-    due_date DATE NOT NULL,
     return_date DATE,
-    condition TEXT,
-    late_fee DECIMAL(10,2)
+    condition VARCHAR(20) CHECK (condition IN ('Good', 'Damaged', 'Lost')),
+    late_fee DECIMAL(3, 2)
 );
