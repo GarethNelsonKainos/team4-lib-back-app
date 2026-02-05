5ab11e2bd1c94d73c254fb961c8c00a110c6dd49
nick-parke <nicholas.parke@kainos.com>
2026-02-05 14:57:38 +0000

    Add DELETE /copies/{id} endpoint, add routes for endpoints

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,36 +1,30 @@
 import { Request, Response } from 'express';
 
-// GET /books
 export const getAllBooks = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
 };
 
-// POST /books
 export const createBook = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
 };
 
-// GET /books/{id}
 export const getBookById = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
 };
 
-// POST /books/{id}
 export const updateBook = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
 };
 
-// DELETE /books/{id}
 export const deleteBook = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
 };
 
-// GET /books/{id}/copies
 export const getBookCopies = async (req: Request, res: Response) => {
   // TODO: Implement logic
   res.status(501).json({ message: 'Not implemented' });
---
+++ src/routes/copies.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+  createCopy,
+  getCopyById,
+  updateCopy,
+  getCopyHistory,
+  deleteCopy
+} from '../controllers/copies.controller';
+
+const router = Router();
+
+router.post('/', createCopy);
+
+router.get('/:id', getCopyById);
+
+router.put('/:id', updateCopy);
+
+router.delete('/:id', deleteCopy);
+
+router.get('/:id/history', getCopyHistory);
+
+export default router;
