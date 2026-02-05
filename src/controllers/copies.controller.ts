b4a9bd971dab402caa2bf1b562f8baea0dd5e137
nick-parke <nicholas.parke@kainos.com>
2026-02-05 14:40:14 +0000

    Implement endpoint skeletons for Books and Copies

---
+++ src/controllers/copies.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+// POST /copies
+export const createCopy = async (req: Request, res: Response) => {
+  // TODO: Implement logic
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+// GET /copies/{id}
+export const getCopyById = async (req: Request, res: Response) => {
+  // TODO: Implement logic
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+// PUT /copies/{id}
+export const updateCopy = async (req: Request, res: Response) => {
+  // TODO: Implement logic
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+// GET /copies/{id}/history
+export const getCopyHistory = async (req: Request, res: Response) => {
+  // TODO: Implement logic
+  res.status(501).json({ message: 'Not implemented' });
+};
